505b9372d694d8649062cba5dc422071dd8a4e87
githamm <khamm@denverpost.com>
2024-06-25 00:14:57 -0600

    Added candidate sorting by total votes, timpestamp, style tweaks

--- 2024/test.py
+++ 2024/test.py
@@ -1,9 +1,9 @@
 import xmltodict
 import json
 
-with open("2024_primary_results.xml") as xml_file:
+with open("detail.xml") as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
 
 json_data = json.dumps(data_dict)
-with open("test_2024.json", "w") as json_file:
+with open("test_2_2024.json", "w") as json_file:
         json_file.write(json_data)
